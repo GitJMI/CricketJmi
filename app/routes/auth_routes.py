cf7d2f7c75b832fe8b6bdc50196add782f9e7e16
GitJMI <anascollegeid@gmail.com>
2026-04-04 23:34:32 +0530

    backend /login and /register issues fix

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,14 +1,21 @@
-from flask import Blueprint, request , jsonify
-from app.services.auth_service import register_user , login_user
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.services.auth_service import register_user, login_user
 
-auth_bp = Blueprint("auth",__name__)
+auth_bp = Blueprint("auth", __name__)
 
-@auth_bp.route("/register",methods =["POST"])
+@auth_bp.route("/register", methods=["POST"])
+@jwt_required(optional=True)
 def register():
+    if get_jwt_identity():
+        return jsonify({"message": "You are already logged in"}), 400
     data = request.json
     return register_user(data)
 
-@auth_bp.route("/login",methods = ["POST"])
+@auth_bp.route("/login", methods=["POST"])
+@jwt_required(optional=True)
 def login():
+    if get_jwt_identity():
+        return jsonify({"message": "You are already logged in"}), 400
     data = request.json
     return login_user(data)
